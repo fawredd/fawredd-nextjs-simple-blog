50a0ed17a957a08a4f644f9375a5b3380f6d5de6
fawredd <moore.marcos@gmail.com>
2025-09-02 13:57:49 -0300

    [fix] dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 import "./globals.css";
 import { footer, brandKeywords } from "@/lib/config";
 import { Analytics } from "@vercel/analytics/next";
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import importPlugin from "eslint-plugin-import";
+import { types } from "util";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,15 +13,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-/*   {
-    plugins: {
-      import: importPlugin
+  {
+    settings: {
+      "import/resolver": {
+          typescript: {},
+          node: {
+            extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"],
+            moduleDirectory: ["node_modules", "./"],
+          },
+      },
     },
     rules: {
       "import/no-unresolved": "error",
-      "import/no-extraneous-dependencies": "error"
-    }
-  } */
+      "import/no-extraneous-dependencies": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
